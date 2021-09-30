07e2f133d954239458dcd6c646524f28fa5d4c93
Evan Poe <evanpoe91@yahoo.com>
2021-09-30 11:00:09 -0500

    fixed bool false issue

--- http-builder.php
+++ http-builder.php
@@ -22,6 +22,40 @@ class parentClass {
 // }
 
 $parent = new parentClass();
-echo $url = http_build_query($parent, '&');
+$url = http_build_query($parent, '&');
 echo $decoded_url = urldecode($url);
+return $decoded_url[0] > 0;
+
+Class A{
+    private $response;
+     
+ 
+    public function runPrimeURL($url) {
+       
+ 
+       $curl = curl_init($url);
+       curl_setopt($curl, CURLOPT_URL, $url);
+       curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+ 
+       $headers = array(
+          "Accept: application/json",
+       );
+       curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+       //for debug only!
+       curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+       curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+ 
+       $resp = curl_exec($curl);
+       curl_close($curl);
+       var_dump($resp);
+ 
+       return $this;
+    }
+ }
+ 
+    $response = new A();
+    $response->runPrimeURL($decoded_url);
+
+
+
 ?>
